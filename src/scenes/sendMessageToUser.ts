6c3ecddf5a716d52cadfc3b6d09422119224d73e
azizjon003 <aliqulovazizjon68@gmail.com>
2024-11-01 01:37:50 +0500

    refactor: Remove unnecessary session state assignment in "Userga xabar yuborish" scene

--- src/scenes/sendMessageToUser.ts
+++ src/scenes/sendMessageToUser.ts
@@ -13,7 +13,7 @@ scene.on("message", async (ctx: any) => {
   if (states === "message") {
     const user = await prisma.user.findFirst({
       where: {
-        id: ctx.scene.session.userId,
+        id: String(ctx.scene.session.userId),
       },
     });
     if (!user) {
