cc79871fe7cf97226889680eb2c9ac417c04b2e3
azizjon003 <aliqulovazizjon68@gmail.com>
2024-11-01 01:33:05 +0500

    refactor: Remove unnecessary session state assignment in "Userga xabar yuborish" scene

--- src/scenes/sendMessageToUser.ts
+++ src/scenes/sendMessageToUser.ts
@@ -1,16 +1,24 @@
-import { Scenes } from "telegraf";
 import prisma from "../../prisma/prisma";
-const scene = new Scenes.BaseScene("send_message_to_user");
-
-scene.hears("/start", async (ctx: any) => {
-  return await ctx.scene.enter("start");
-});
+import scene from "./address";
 
 scene.on("message", async (ctx: any) => {
   const text = ctx.message.text;
   const states = ctx.scene.session.state;
 
-  if (states) {
+  if (states === "message") {
+    // Xabar yuborish logikasi
+    const user = await prisma.user.findFirst({
+      where: {
+        id: ctx.scene.session.userId,
+      },
+    });
+    if (!user) {
+      return ctx.reply("Foydalanuvchi topilmadi");
+    }
+    await ctx.telegram.sendMessage(user.telegram_id, text);
+    return ctx.reply("Xabar yuborildi");
+  } else {
+    // User ID ni tekshirish va saqlash logikasi
     const userId = Number(text);
     const user = await prisma.user.findFirst({
       where: {
@@ -24,27 +32,6 @@ scene.on("message", async (ctx: any) => {
       state: "message",
       userId,
     };
-    ctx.scene.session.userId = userId;
     return ctx.reply("Xabar matnini kiriting");
-  } else if (states === "message") {
-    const user = await prisma.user.findFirst({
-      where: {
-        id: ctx.scene.session.userId,
-      },
-    });
-    if (!user) {
-      return ctx.reply("Foydalanuvchi topilmadi");
-    }
-    await ctx.telegram.sendMessage(user.telegram_id, text);
-    return ctx.reply("Xabar yuborildi");
   }
-  return ctx.scene.enter("start");
 });
-
-const sleep = async (ms: number) => {
-  return new Promise((resolve) => {
-    setTimeout(resolve, ms);
-  });
-};
-
-export default scene;
